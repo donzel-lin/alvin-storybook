3d7f7928dec4c80abb606e752f0dcbc286950b0f
Your Name <you@example.com>
2022-08-17 16:18:38 +0800

    init

---
+++ packages/al-input/al-input.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<input
+			v-bind="$attrs"
+			@input="handleInput"
+			@change="handleChange"
+			@blur="handleBlur"
+		/>
+	</div>
+</template>
+<script>
+export default {
+	name: 'AlInput',
+	inject: ['AlFormItem'],
+	methods: {
+		handleInput(evt) {
+			this.$emit('update:value', evt.target.value);
+			this.$emit('input', evt.target.value);
+			this.AlFormItem.validate(evt.target.value);
+		},
+		handleChange(evt) {
+			this.$emit('update:value', evt.target.value);
+			this.$emit('change', evt.target.value);
+			this.AlFormItem.validate(evt.target.value);
+		},
+		handleBlur(evt) {
+			this.$emit('update:value', evt.target.value);
+			this.$emit('blur', evt.target.value);
+			this.AlFormItem.validate(evt.target.value);
+		},
+	},
+};
+</script>
